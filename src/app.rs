c92a6bcfa7ce13d3e1fd704f19455a7a49572735
Tyler McCormick <enzo@Tylers-MacBook-Pro.local>
2023-08-05 23:52:17 -0600

    sorry

--- src/app.rs
+++ src/app.rs
@@ -1,6 +1,13 @@
 use egui_extras::RetainedImage;
+use notify::{RecommendedWatcher, RecursiveMode, Watcher};
+use std::path::Path;
 use walkdir::WalkDir;
+use std::cell::RefCell;
+use std::rc::Rc;
+
 pub struct ImageSequencerApp {
+    watcher: Option<RecommendedWatcher>,
+    directory: Option<String>,
     images: Vec<String>,
     idx: usize,
 }
@@ -8,6 +15,8 @@ pub struct ImageSequencerApp {
 impl Default for ImageSequencerApp {
     fn default() -> Self {
         Self {
+            watcher: None,
+            directory: None,
             images: Vec::new(),
             idx: 0,
         }
@@ -22,6 +31,27 @@ impl ImageSequencerApp {
 }
 
 impl ImageSequencerApp {
+    fn watch_directory(&mut self, directory: &str) {
+        let self_rc = Rc::new(RefCell::new(self.clone()));
+        let mut watcher = notify::recommended_watcher(move |res: Result<notify::event::Event, notify::Error>| match res {
+            Ok(event) => match event.kind {
+                notify::EventKind::Modify(_) => {
+                    let mut self_ref = self_rc.borrow_mut();
+                    self.load_images_from_directory(directory);
+                }
+                _ => {}
+            },
+            Err(e) => println!("watch error: {:?}", e),
+        })
+        .unwrap();
+
+        watcher
+            .watch(Path::new(directory), RecursiveMode::Recursive)
+            .unwrap();
+
+        self.watcher = Some(watcher);
+    }
+
     fn load_images_from_directory(&mut self, directory: &str) {
         self.images.clear();
 
@@ -58,7 +88,6 @@ impl eframe::App for ImageSequencerApp {
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
         #[cfg(not(target_arch = "wasm32"))] // no File->Quit on web pages!
         egui::TopBottomPanel::top("top_panel").show(ctx, |ui| {
-            // The top panel is often a good place for a menu bar:
             egui::menu::bar(ui, |ui| {
                 ui.menu_button("File", |ui| {
                     if ui.button("Quit").clicked() {
@@ -69,7 +98,12 @@ impl eframe::App for ImageSequencerApp {
         });
 
         egui::CentralPanel::default().show(ctx, |ui| {
-            if !self.images.is_empty() {
+            if self.directory.is_some() {
+                ui.heading(format!(
+                    "Current Directory: {}",
+                    self.directory.as_ref().unwrap()
+                ));
+
                 if let Some(img) = self.load_current_image() {
                     ui.add(egui::Image::new(img.texture_id(ctx), img.size_vec2()));
 
@@ -91,7 +125,9 @@ impl eframe::App for ImageSequencerApp {
                 match result {
                     nfd::Response::Okay(path) => {
                         println!("Path = {:?}", path);
+                        self.watch_directory(path.as_str());
                         self.load_images_from_directory(path.as_str());
+                        self.directory = Some(path);
                     }
                     nfd::Response::OkayMultiple(paths) => {
                         println!("Paths = {:?}", paths);
@@ -100,6 +136,11 @@ impl eframe::App for ImageSequencerApp {
                 }
             }
 
+            if !self.images.is_empty() {
+                if ui.button("Reload Directory").clicked() {}
+            }
+
+            // stops the animation but doesn't clear directory
             if ui.button("Stop").clicked() {
                 self.images.clear();
             }
